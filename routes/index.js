3d4afbcf654410b3daa44b6236ad748ef40cc552
scheuerkev <scheuer.kevin@gmail.com>
2021-12-29 18:10:00 +0100

    Reformating views

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,14 @@
 const userRoutes = require("./user.routes");
 const authRoutes = require("./auth.routes");
-const { ensureAuthenticated } = require("../config/security.config");
 const router = require("express").Router();
 
 router.use("/users", userRoutes);
 router.use("/auth", authRoutes);
 
-router.get("/protected", ensureAuthenticated, (req, res) => {
-  res.render("protected");
-});
-
 router.get("/", (req, res) => {
-  res.render("index", { user: null });
+  req.isAuthenticated()
+    ? res.render("index", { user: req.user })
+    : res.redirect("/auth/signin/form");
 });
 
 module.exports = router;
